c5a1e0d1974b7280d3442029dbaa1a50b08aa1fa
Richard Mathis <richard.mathis@sonarsource.com>
2021-03-31 11:24:53 +0200

    test @ComponentScan

--- src/main/java/com/mathiric/house/HouseApplication.java
+++ src/main/java/com/mathiric/house/HouseApplication.java
@@ -2,13 +2,16 @@ package com.mathiric.house;
 
 //import com.mathiric.house.controller.HouseController;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
+@EnableAutoConfiguration
 @ComponentScan("com.mathiric.house.domain")
 //@SpringBootApplication("com.mathiric.house.domain")
+//@SpringBootApplication
 public class HouseApplication {
 
 	public static void main(String[] args) {
